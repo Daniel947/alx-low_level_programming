915ebcf8453eb33dfa9299b0a547c7045b1a2793
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-04 12:50:43 -0700

    Alphabet print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-/*
- * main - Entry function.
- *
- * Return: value 0.
- *
- * Author: Daniel Yamoah.
- *
- *
- */
+/**
+* main - Entry function.
+*
+* Return: value 0.
+*
+* Author: Daniel Yamoah.
+*
+*/
 int main(void)
 {
-        int numberz;
-
-        for (numberz = '0'; numberz <= '9'; numberz++)
-        {
-                putchar(numberz);
-
-        }
-        putchar('\n');
-        return (0);
+	int numberz;
+	
+	for (numberz = '0'; numberz <= '9'; numberz++)
+	{
+		putchar(numberz);
+	}
+	putchar('\n');
+	return (0);
 }
