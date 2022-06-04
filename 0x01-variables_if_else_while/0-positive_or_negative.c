17e8ab0120d840a65ec2dd8694ca12899054c06c
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-03 21:22:19 -0700

    Alx low level programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	/*Print the zero value of n*/
-	if (n==0){
-		printf("%d is zero",n);	
+	if (n == 0)
+	{
+		printf("%d is zero\n",n);	
 	}
 	/*Print the negative value of n*/
-	if(n<0){
-		printf("%d is negative",n);
+	if(n < 0)
+	{
+		printf("%d is negative\n",n);
 	}
 	/*Print the positive value of n*/
-	else{
+	else
 		printf("%d is positive\n",n);
-	}
+	
 	return (0);
 }
