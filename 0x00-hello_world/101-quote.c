01d4dea1897898ab27952d721c57c256cc63973d
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-06 21:13:51 -0700

    extra fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main- Entry point
  *
@@ -8,6 +9,8 @@
  */
 int main()
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015\-10\-19");
+	char string[] = "and that piece of art is useful \" - Dora Korpar, 2015-10-19";
+
+	write(2,string,sizeof(string));	
 	return (1);
 }
