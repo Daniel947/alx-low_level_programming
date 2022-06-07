4c063c5f639f1d08f8e8b0b11dca2864d9974aa1
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-06 21:02:52 -0700

    Extra fixing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: 0 (success)
+ *
+ * Author: Daniel Yamoah
+ */
+int main()
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
