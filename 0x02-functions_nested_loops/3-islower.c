91cf38c6c22297e91bfe25adc21668f7bb7348ea
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-09 16:44:25 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "codestd.h"
+/**
+ * _islower - Entry function
+ *
+ * Return: always 0 (success).
+ *
+ * Author: Daniel Yamoah
+ */
+int _islower(int c)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == c)
+		{
+			return(1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
+}
