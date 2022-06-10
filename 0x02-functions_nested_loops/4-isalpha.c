7044457ce0c9257a115e7eafc1c8b2081d207572
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-09 19:56:54 -0700

    Function nested loop

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - main function entry
+ * @c : letter checker
+ * Return: 1 if c is lower or upper
+ * 0 if c anything else
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c >='A' && c <= 'z' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
