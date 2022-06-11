6df9920c9f836815f16b8f3a98618fc3795a1a1d
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-11 11:59:11 -0700

    Update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,16 @@
  */
 void print_to_98(int n)
 {
-        while (n < 98)
-        {
-                printf("%i ,", n);
-                n++;
-        }
-        while (n > 98)
-        {
-                printf("%i ,", n);
-                n--;
-        }
-        printf("98");
-        _putchar('\n');
+	while (n < 98)
+	{
+		printf("%i ,", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i ,", n);
+		n--;
+	}
+	printf("98");
+	_putchar('\n');
 }
