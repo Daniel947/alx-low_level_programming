0c490311e60dc298dc0f6fb79c200ae7e7e7cea1
Daniel Yamoah <owusudaniel4422@gmail.com>
2022-06-18 07:55:26 -0700

    More on function nested loop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
  * _isdigit - function for checking if input is digit
  *
  * Return: 0 or 1.
- * @c: input parameter  for _isdigit 
+ * @c: input parameter  for _isdigit
  * Author: Daniel Yamoah
  */
 int _isdigit(int c)
